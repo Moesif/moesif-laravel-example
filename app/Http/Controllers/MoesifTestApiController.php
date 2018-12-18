6d42f9c37191d78ddde8b58665fdddf9735aea00
xinghengwang <xing@moesif.com>
2018-12-18 13:42:51 -0800

    update: added test cases for large data set.

--- app/Http/Controllers/MoesifTestApiController.php
+++ app/Http/Controllers/MoesifTestApiController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 
+use Illuminate\Support\Facades\Log;
+
 use App\Http\Requests;
+use Moesif\Sender\MoesifApi;
 
 class MoesifTestApiController extends Controller
 {
@@ -17,10 +20,27 @@ class MoesifTestApiController extends Controller
      */
     public function index($v)
     {
+        // $moesifApi = MoesifApi::getInstance('eyJhcHAiOiIyMzc6NzkiLCJ2ZXIiOiIyLjAiLCJvcmciOiI2Mjg6NTMiLCJpYXQiOjE1Mzk4MjA4MDB9.g9qvm5y80mHNtqh5KkG4f6YTErP59-8JHotipsusNuM',  ['fork'=>true, 'debug'=>true]);
+
+        Log::info('[Moesif] : obtainting $moesifAPi ');
+
+        $moesifApi = MoesifApi::getInstance('your application id',  ['fork'=>true, 'debug'=>true]);
+
+
+        $moesifApi->updateUser([
+          'user_id' => 'updateuser_test1',
+          'metadata' => [
+            'name' => 'xing',
+            'email' => 'xing@updateusertest.com',
+            'special_value' => $v
+          ]
+        ]);
+
         return new JsonResponse([
             "myname" => "xing",
             "state" => "dude",
-            "value" => $v
+            "value" => $v,
+            "moesifApi_null" => is_null($moesifApi)
         ]);
     }
 
@@ -34,6 +54,17 @@ class MoesifTestApiController extends Controller
 
     public function bigjson()
     {
+
+      $moesifApi = MoesifApi::getInstance('eyJhcHAiOiIyMzc6NzkiLCJ2ZXIiOiIyLjAiLCJvcmciOiI2Mjg6NTMiLCJpYXQiOjE1Mzk4MjA4MDB9.g9qvm5y80mHNtqh5KkG4f6YTErP59-8JHotipsusNuM',  ['fork'=>true, 'debug'=>true]);
+
+      $moesifApi->updateUser([
+        'user_id' => 'test_bigjson',
+        'metadata' => [
+          'name' => 'xing',
+          'email' => 'xing@updateusertest.com',
+          'special_value' => 'test big json'
+        ]
+      ]);
       // we only support body size up to 100Kb.
       // so 2000 entries works. but
       // 3000 entries and above probalby wont work
